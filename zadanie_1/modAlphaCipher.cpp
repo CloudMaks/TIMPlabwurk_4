177f96634d8c14479c206dcfe4839adb555bc676
CloudMaks <81799531+CloudMaks@users.noreply.github.com>
2021-06-04 17:42:28 +0300

    Update modAlphaCipher.cpp

--- zadanie_1/modAlphaCipher.cpp
+++ zadanie_1/modAlphaCipher.cpp
@@ -1,7 +1,9 @@
 #include "modAlphaCipher.h"
+
 modAlphaCipher::modAlphaCipher(wstring skey)
 {
-    for (unsigned i=0; i<numAlpha.size(); i++) {
+    for (unsigned i=0; i<numAlpha.size(); i++) 
+    {
         alphaNum[numAlpha[i]]=i;
     }
     key = convert(get_val_kl(skey));
@@ -9,7 +11,8 @@ modAlphaCipher::modAlphaCipher(wstring skey)
 wstring modAlphaCipher::encrypt(const wstring& open_text)
 {
     vector<int> work = convert(get_val_txt(open_text));
-    for(unsigned i=0; i < work.size(); i++) {
+    for(unsigned i=0; i < work.size(); i++) 
+    {
         work[i] = (work[i] + key[i % key.size()]) % alphaNum.size();
     }
     return convert(work);
@@ -17,7 +20,8 @@ wstring modAlphaCipher::encrypt(const wstring& open_text)
 wstring modAlphaCipher::decrypt(const wstring& cipher_text)
 {
     vector<int> work = convert(get_val_txt(cipher_text));
-    for(unsigned i=0; i < work.size(); i++) {
+    for(unsigned i=0; i < work.size(); i++) 
+    {
         work[i] = (work[i] + alphaNum.size() - key[i % key.size()]) % alphaNum.size();
     }
     return convert(work);
@@ -25,7 +29,8 @@ wstring modAlphaCipher::decrypt(const wstring& cipher_text)
 inline vector<int> modAlphaCipher::convert(const wstring& s)
 {
     vector<int> result;
-    for(auto c:s) {
+    for(auto c:s) 
+    {
         result.push_back(alphaNum[c]);
     }
     return result;
@@ -33,7 +38,8 @@ inline vector<int> modAlphaCipher::convert(const wstring& s)
 inline wstring modAlphaCipher::convert(const vector<int>& v)
 {
     wstring result;
-    for(auto i:v) {
+    for(auto i:v)
+    {
         result.push_back(numAlpha[i]);
     }
     return result;
@@ -45,12 +51,15 @@ inline wstring modAlphaCipher::get_val_kl(const wstring & s)
     if (s.empty())
         throw exc(string("вы ввели пустой ключ."));
     wstring wst = s;
-    for (auto & el : wst) {
-        if (iswalpha(el)==0) {
+    for (auto & el : wst) 
+    {
+        if (iswalpha(el)==0) 
+        {
             throw exc(string("в вашем ключе присутствуют недопустимые символы."));
             break;
         }
-        if (iswlower(el)) {
+        if (iswlower(el)) 
+        {
         el = towupper(el);
         }
     }
@@ -64,12 +73,15 @@ inline wstring modAlphaCipher::get_val_txt(const wstring & s)
     if (s.empty())
         throw exc(string("Был введен пустой текст."));
     wstring wst = s;
-    for (auto & el : wst) {
-        if (iswalpha(el)==0) {
+    for (auto & el : wst)
+    {
+        if (iswalpha(el)==0) 
+        {
             throw exc (string("Текст должен состоять только из строки русских букв."));
             break;
         }
-        if (iswlower(el)) {
+        if (iswlower(el))
+        {
         el = towupper(el);
         }
     }
