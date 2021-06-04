dc80b2d15638222eb9f48884b6287f78e45ffcbe
CloudMaks <81799531+CloudMaks@users.noreply.github.com>
2021-06-04 17:43:04 +0300

    Update exception.h

--- zadanie_1/exception.h
+++ zadanie_1/exception.h
@@ -9,6 +9,7 @@
 #pragma once
 #include <stdexcept>
 #include <string>
+
 using namespace std;
 /// @brief  Класс для обработки ошибок, которые могут возникнуть при взаимодействии с программой
 /// @details Класс наследует существующий класс обработки исключений с именем "invalid_argument" из библиотеки "stdexcept".
